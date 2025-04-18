3e9eb9a2335f29ee15fc237d08bbfdcf5d0399d3
__Humanoid__ <88325003+DTNhan0@users.noreply.github.com>
2025-04-19 04:09:45 +0700

    Update ThanhToanController.java
    done

--- src/main/java/com/banHangLKDT/controller/ThanhToanController.java
+++ src/main/java/com/banHangLKDT/controller/ThanhToanController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1")
 public class ThanhToanController {
     @Autowired
